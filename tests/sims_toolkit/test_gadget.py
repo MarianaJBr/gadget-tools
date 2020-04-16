8d0627a93a7868b104122c2ef225aa7363b916de
Omar Abel Rodríguez-López <oarodriguez.mx@gmail.com>
2020-04-16 00:44:27 -0500

    ✅🎨 Improve fixture to indicate snapshot format through environment variables

--- tests/sims_toolkit/test_gadget.py
+++ tests/sims_toolkit/test_gadget.py
@@ -1,5 +1,4 @@
 import os
-import pathlib
 
 import attr
 import pytest
@@ -10,45 +9,43 @@ from sims_toolkit.gadget.snapshot import Header
 load_dotenv()
 
 
-@pytest.fixture
-def snapshot_path():
-    """Return the path of a testing snapshot file."""
-    path_env_var = os.getenv("ST_TEST_SNAPSHOT")
-    snapshot_path = os.path.abspath(os.path.expanduser(path_env_var))
-    if not os.path.exists(snapshot_path):
-        pytest.skip("snapshot file not available")
-    return pathlib.Path(snapshot_path)
-
-
-@pytest.fixture
-def alt_snapshot_path():
-    """Return the path of a testing snapshot file."""
-    path_env_var = os.getenv("ST_TEST_SNAPSHOT_NO_ALT")
-    snapshot_path = os.path.abspath(os.path.expanduser(path_env_var))
-    if not os.path.exists(snapshot_path):
-        pytest.skip("snapshot file not available")
-    return pathlib.Path(snapshot_path)
-
-
-def test_load_snapshot(snapshot_path):
+@attr.s(auto_attribs=True)
+class SnapshotSpec:
     """"""
-    with open(snapshot_path, "rb") as g2fp:
-        snapshot_data = load_snapshot(g2fp)
+    path: str
+    alt_snap_format: bool = True
+
+
+@pytest.fixture
+def snapshot_spec():
+    """Return the path of a testing snapshot file."""
+    alt_snap_format = bool(os.getenv("ST_ALT_SNAP_FORMAT"))
+    if alt_snap_format:
+        path_env_var = os.getenv("ST_TEST_SNAPSHOT")
+    else:
+        path_env_var = os.getenv("ST_TEST_SNAPSHOT_NO_ALT")
+    snapshot_path = os.path.abspath(os.path.expanduser(path_env_var))
+    if not os.path.exists(snapshot_path):
+        pytest.skip("snapshot file not available")
+    return SnapshotSpec(snapshot_path, alt_snap_format)
+
+
+def test_load_snapshot(snapshot_spec):
+    """"""
+    path = snapshot_spec.path
+    alt_snap_format = snapshot_spec.alt_snap_format
+    with open(path, "rb") as g2fp:
+        snapshot_data = load_snapshot(g2fp, alt_snap_format=alt_snap_format)
     print(snapshot_data)
 
 
-def test_load_snapshot_no_alt(alt_snapshot_path):
+def test_load_header(snapshot_spec):
     """"""
-    with open(alt_snapshot_path, "rb") as g2fp:
+    path = snapshot_spec.path
+    alt_snap_format = snapshot_spec.alt_snap_format
+    with open(path, "rb") as g2fp:
         snapshot_data = load_snapshot(g2fp, blocks=(),
-                                      alt_snap_format=False)
-    print(snapshot_data)
-
-
-def test_load_header(snapshot_path):
-    """"""
-    with open(snapshot_path, "rb") as g2fp:
-        snapshot_data = load_snapshot(g2fp, blocks=())
+                                      alt_snap_format=alt_snap_format)
     snapshot_data_dict = attr.asdict(snapshot_data, recurse=False)
     header = snapshot_data_dict.pop("header")
     assert header is not None
@@ -56,10 +53,13 @@ def test_load_header(snapshot_path):
         assert block is None
 
 
-def test_header_as_data(snapshot_path):
+def test_header_as_data(snapshot_spec):
     """"""
-    with open(snapshot_path, "rb") as g2fp:
-        snapshot_data = load_snapshot(g2fp, blocks=())
+    path = snapshot_spec.path
+    alt_snap_format = snapshot_spec.alt_snap_format
+    with open(path, "rb") as g2fp:
+        snapshot_data = load_snapshot(g2fp, blocks=(),
+                                      alt_snap_format=alt_snap_format)
     header = snapshot_data.header
     header_as_data = header.as_data()
     assert header == Header.from_data(header_as_data)
