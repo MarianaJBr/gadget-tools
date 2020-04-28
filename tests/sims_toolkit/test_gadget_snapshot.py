2128439a279033d90e3c875f5483436c8e124ed3
Omar Abel Rodríguez-López <oarodriguez.mx@gmail.com>
2020-04-28 13:18:19 -0500

    🎨♻ Add tests

--- tests/sims_toolkit/test_gadget_snapshot.py
+++ tests/sims_toolkit/test_gadget_snapshot.py
@@ -1,9 +1,14 @@
 import os
+from pathlib import Path
 
+import numpy as np
 import pytest
 from dotenv import load_dotenv
 from sims_toolkit.gadget import File
-from sims_toolkit.gadget.snapshot import FileFormat
+from sims_toolkit.gadget.snapshot import (
+    Block, BlockData, FileFormat, Header,
+    header_dtype
+)
 
 load_dotenv()
 
@@ -11,21 +16,17 @@ load_dotenv()
 @pytest.fixture
 def snapshot_path():
     """Return the path of a testing snapshot file."""
-    alt_snap_format = bool(os.getenv("ST_ALT_SNAP_FORMAT"))
-    if alt_snap_format:
-        path_env_var = os.getenv("ST_TEST_SNAPSHOT")
-    else:
-        path_env_var = os.getenv("ST_TEST_SNAPSHOT_NO_ALT")
-    snapshot_path = os.path.abspath(os.path.expanduser(path_env_var))
-    if not os.path.exists(snapshot_path):
+    path_env_var = os.getenv("ST_TEST_SNAPSHOT")
+    snap_path = os.path.abspath(os.path.expanduser(path_env_var))
+    if not os.path.exists(snap_path):
         pytest.skip("snapshot file not available")
-    return snapshot_path
+    return snap_path
 
 
 def test_read(snapshot_path):
     """"""
-    with File(snapshot_path) as snapshot:
-        print(snapshot)
+    with File(snapshot_path) as snap:
+        print(snap)
 
 
 def test_read_invalid_mode(snapshot_path):
@@ -33,24 +34,24 @@ def test_read_invalid_mode(snapshot_path):
     with pytest.raises(ValueError):
         # Although snapshots are open as binary, the mode does not accept
         # the ``b`` modifier.
-        with File(snapshot_path, "rb") as snapshot:
-            print(snapshot)
+        with File(snapshot_path, "rb") as snap:
+            print(snap)
 
 
 def test_inspect(snapshot_path):
     """"""
-    with File(snapshot_path) as snapshot:
-        block_specs = snapshot.inspect()
+    with File(snapshot_path) as snap:
+        block_specs = snap.inspect()
         print(block_specs)
 
 
 def test_load_blocks(snapshot_path):
     """"""
-    with File(snapshot_path) as snapshot:
-        positions1 = snapshot["POS"]
-        velocities = snapshot["ID"]
-        positions2 = snapshot["POS"]
-        print(snapshot.header)
+    with File(snapshot_path) as snap:
+        positions1 = snap["POS"]
+        velocities = snap["ID"]
+        positions2 = snap["POS"]
+        print(snap.header)
         print(positions1)
         print(positions2)
         print(velocities)
@@ -58,17 +59,17 @@ def test_load_blocks(snapshot_path):
 
 def test_iter(snapshot_path):
     """"""
-    with File(snapshot_path) as snapshot:
-        for block in snapshot.values():
+    with File(snapshot_path) as snap:
+        for block in snap.values():
             print("************ Block ************")
             print(block)
-        print("Number of reachable snapshot blocks: {}".format(len(snapshot)))
+        print("Number of reachable snapshot blocks: {}".format(len(snap)))
 
 
-def test_copy_snapshot(snapshot_path):
+def test_copy(snapshot_path):
     """"""
     with File(snapshot_path) as snap:
-        new_snap_path = snapshot_path + "-test-snap"
+        new_snap_path = snapshot_path + "-test-copy"
         with File(new_snap_path, "w") as new_snap:
             # First assign the header.
             new_snap.header = snap.header
@@ -78,10 +79,72 @@ def test_copy_snapshot(snapshot_path):
             new_snap["ID"] = snap["ID"]
 
 
-def test_copy(snapshot_path):
+def test_compare_copy(snapshot_path):
+    """"""
     with File(snapshot_path) as snap:
-        new_snap_path = snapshot_path + "-test-snap"
+        new_snap_path = snapshot_path + "-test-copy"
         with File(new_snap_path) as new_snap:
             assert new_snap.format is FileFormat.ALT
             assert new_snap.header == snap.header
             print(new_snap.inspect())
+
+
+def test_copy_default_format(snapshot_path):
+    """"""
+    with File(snapshot_path) as snap:
+        new_snap_path = snapshot_path + "-test-copy"
+        with File(new_snap_path, "w", format=FileFormat.DEFAULT) as new_snap:
+            # First assign the header.
+            new_snap.header = snap.header
+            # Block assignment order matters.
+            new_snap["POS"] = snap["POS"]
+            new_snap["VEL"] = snap["VEL"]
+            new_snap["ID"] = snap["ID"]
+
+
+def test_compare_copy_default_format(snapshot_path):
+    """"""
+    with File(snapshot_path) as snap:
+        new_snap_path = snapshot_path + "-test-copy"
+        with File(new_snap_path) as new_snap:
+            assert new_snap.format is FileFormat.DEFAULT
+            assert new_snap.header == snap.header
+            print(new_snap.inspect())
+
+
+def test_copy_block_twice(snapshot_path):
+    """"""
+    with pytest.raises(ValueError):
+        with File(snapshot_path) as snap:
+            new_snap_path = snapshot_path + "-test-snap"
+            with File(new_snap_path, "w") as new_snap:
+                # First assign the header.
+                new_snap.header = snap.header
+                # This must fail.
+                new_snap["POS"] = snap["POS"]
+                new_snap["POS"] = snap["POS"]
+
+
+def test_create():
+    """"""
+    file_path = Path(os.path.join(os.getcwd(), "test-dummy-snap"))
+    with File(file_path, "w") as snap:
+        # Header attributes from a real snapshot.
+        header_attrs = ((2479138, 2539061, 0, 0, 99736, 0),
+                        (0.0, 0.053677940511483495, 0.0, 0.0, 0.0, 0.0),
+                        0.24999999949102492, 3.0000000081436013, 1, 1,
+                        (125791124, 134217728, 0, 0, 8426604, 0), 1, 64,
+                        100000.0, 0.308, 0.692, 0.678)
+        header_data = np.array(header_attrs, dtype=header_dtype)
+        # Assign new header.
+        header = Header.from_data(header_data)
+        snap.header = header
+        # Fake position data.
+        num_par_halo = header.num_par_spec.halo
+        pos_attrs = {
+            "gas": np.random.random_sample((num_par_halo, 3))
+        }
+        block_data = BlockData(**pos_attrs)
+        pos_block = Block(id="POS", data=block_data)
+        # Assign new block.
+        snap["POS"] = pos_block
